eca4ca77a289ca274df218a4d93e5b5fa2ab3f18
Hammad <dpxpayofficial@gmail.com>
2025-12-09 11:31:49 +0500

    Fix: remove AppProps/pageProps error in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+// pages/index.tsx
+import type { NextPage } from 'next';
+
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Tokenomics from '../components/Tokenomics';
-import Roadmap from '../components/Roadmap';
 import Staking from '../components/Staking';
-import Community from '../components/Community';
+import Roadmap from '../components/Roadmap';
 import FAQ from '../components/FAQ';
+import Community from '../components/Community';
 import Footer from '../components/Footer';
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
       <Tokenomics />
-      <Roadmap />
       <Staking />
-      <Community />
+      <Roadmap />
       <FAQ />
+      <Community />
       <Footer />
     </>
   );
-}
+};
+
+export default Home;
